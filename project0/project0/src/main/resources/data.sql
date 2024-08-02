0b7ebf46c7db02a25f733e9cf08a5521b4bf8616
Josh Williams <j.williams219@outlook.com>
2024-08-02 12:06:42 -0600

    add h2 db persistence

---
+++ project0/project0/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+
+Insert Into Collector (username, password) Values ('user', 'pass');
+Insert Into Collector (username, password) Values ('user2', 'pass');
+Insert Into Collector (username, password) Values ('user3', 'pass');
+Insert Into Collector (username, password) Values ('user4', 'pass');
