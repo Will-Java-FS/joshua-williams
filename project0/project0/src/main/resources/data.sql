20e1488d02a81f63349ef0ab548456685b9c33bf
Emmett Duffy <redpad@wetsnow.com>
2024-08-05 15:26:49 -0400

    Pokemon Database

--- project0/project0/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-
-Insert Into Collector (username, password) Values ('user', 'pass');
-Insert Into Collector (username, password) Values ('user2', 'pass');
-Insert Into Collector (username, password) Values ('user3', 'pass');
-Insert Into Collector (username, password) Values ('user4', 'pass');
